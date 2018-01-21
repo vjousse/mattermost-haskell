e11d31f10c50aef45ae93667de6bbe2594d5de1b
Vincent Jousse <vincent@jousse.org>
2018-01-21 22:40:20 +0100

    Display token in Main app

--- app/Main.hs
+++ app/Main.hs
@@ -8,8 +8,8 @@ import           Data.Aeson         (ToJSON, object, pairs, toEncoding, toJSON,
 import           Data.Aeson.Lens    (key, _String)
 import           Data.ByteString    (unpack)
 import           Data.Text.Encoding (decodeUtf8)
-import           Mattermost
-import           Mattermost.Data    (Credentials (Credentials))
+import           Mattermost         (login)
+import           Mattermost.Data    (Credentials (Credentials), Token)
 import           Network.Wreq       (get, post, responseBody, responseHeader,
                                      responseStatus, statusMessage)
 import           System.Environment (getEnv)
@@ -23,6 +23,8 @@ doPost = do
   clientId <- getEnv "LOGIN_ID"
   password <- getEnv "PASSWORD"
   url <- getEnv "URL"
-  r <- post url (toJSON $ Credentials clientId password)
-  putStrLn $ show $ r CL.^. responseBody . key "username" . _String
-  putStrLn $ show $ r CL.^. responseHeader "token"
+  response <- login clientId password url
+  --print (r CL.^. responseBody . key "username" . _String)
+  case response of
+    Right t -> putStrLn t
+    Left e  -> putStrLn $ "Error while retrieving token: " ++ e
