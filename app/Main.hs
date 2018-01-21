b9469670f256b242f87abeb8a9325a7fd87f5b78
Vincent Jousse <vincent@jousse.org>
2018-01-21 22:48:01 +0100

    Pedantic time

--- app/Main.hs
+++ app/Main.hs
@@ -2,16 +2,7 @@
 
 module Main where
 
-import qualified Control.Lens       as CL ((^.), (^?))
-import           Data.Aeson         (ToJSON, object, pairs, toEncoding, toJSON,
-                                     (.=))
-import           Data.Aeson.Lens    (key, _String)
-import           Data.ByteString    (unpack)
-import           Data.Text.Encoding (decodeUtf8)
 import           Mattermost         (login)
-import           Mattermost.Data    (Credentials (Credentials), Token)
-import           Network.Wreq       (get, post, responseBody, responseHeader,
-                                     responseStatus, statusMessage)
 import           System.Environment (getEnv)
 
 main :: IO ()
@@ -24,7 +15,6 @@ doPost = do
   password <- getEnv "PASSWORD"
   url <- getEnv "URL"
   response <- login clientId password url
-  --print (r CL.^. responseBody . key "username" . _String)
   case response of
     Right t -> putStrLn t
     Left e  -> putStrLn $ "Error while retrieving token: " ++ e
--- src/Mattermost.hs
+++ src/Mattermost.hs
@@ -9,12 +9,9 @@ import qualified Control.Exception     as E
 import qualified Control.Lens          as CL ((^.))
 import           Data.Aeson            (toJSON)
 import qualified Data.ByteString.Char8 as BSC
-import qualified Data.ByteString.Lazy  as LBS
-import           GHC.Generics
 import           Mattermost.Data       (Credentials (Credentials), Login,
                                         Password, Token, Url)
-import           Network.HTTP.Client   (HttpException (HttpExceptionRequest), HttpExceptionContent (StatusCodeException),
-                                        Response)
+import           Network.HTTP.Client   (HttpException (HttpExceptionRequest), HttpExceptionContent (StatusCodeException))
 import           Network.Wreq          (post, responseHeader, responseStatus,
                                         statusMessage)
 
@@ -23,8 +20,10 @@ login loginId password url =
   (Right . show . (CL.^. responseHeader "token") <$>
    post url (toJSON $ Credentials loginId password)) `E.catch`
   handler
+  --(r CL.^. responseBody . key "username" . _String)
   where
     handler :: HttpException -> IO (Either String Token)
     handler (HttpExceptionRequest _ (StatusCodeException r _)) =
       return . Left . show . BSC.unpack $
       (r CL.^. responseStatus . statusMessage)
+    handler _ = return . Left $ "Unhandled exception"
