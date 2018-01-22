6018f215d2b191bb1319c7405b12d9a3b6d447c0
Vincent Jousse <vincent@jousse.org>
2018-01-22 09:08:35 +0100

    Add some comments for the future me

--- src/Mattermost.hs
+++ src/Mattermost.hs
@@ -18,6 +18,12 @@ import           Network.Wreq          (post, responseHeader, responseStatus,
 login :: Login -> Password -> Url -> IO (Either String Token)
 login loginId password url =
   (Right . show . (CL.^. responseHeader "token") <$>
+  -- Function composition from right to left
+  -- 1. Apply (CL.^. responseHeader "token"): get the token header from the response
+  -- 2. Apply show: transform ByteString to String
+  -- 3. Apply Right: put everything in an Either
+  -- <$> Applicative operator, Apply the function on the left to the Applicative
+  -- on the right. Gives IO (Either …), the Either is applied inside the IO
    post url (toJSON $ Credentials loginId password)) `E.catch`
   handler
   --(r CL.^. responseBody . key "username" . _String)
@@ -25,5 +31,7 @@ login loginId password url =
     handler :: HttpException -> IO (Either String Token)
     handler (HttpExceptionRequest _ (StatusCodeException r _)) =
       return . Left . show . BSC.unpack $
+        -- return wraps what's on the right in the same Monad
+        -- In our case, the Monad is IO, return by (r CL.^. responseStatus . statusMessage)
       (r CL.^. responseStatus . statusMessage)
     handler _ = return . Left $ "Unhandled exception"
